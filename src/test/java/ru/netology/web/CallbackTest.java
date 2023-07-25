7d7aefb1175582ac4eeae17fa2bb77ab46afd854
Petrov Maxim <maxim.petrov@1ci.com>
2023-07-25 22:40:30 +0300

    test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -40,16 +40,16 @@ class CallbackTest {
         driver = null;
     }
 
-    @Test
-    public void shouldSuccessTestV1() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        driver.findElements(By.className("input__control")).get(0).sendKeys("Максим");
-        driver.findElements(By.className("input__control")).get(1).sendKeys("+79273569501");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
-    }
+//    @Test
+//    public void shouldSuccessTestV1() {
+//        List<WebElement> elements = driver.findElements(By.className("input__control"));
+//        driver.findElements(By.className("input__control")).get(0).sendKeys("Максим");
+//        driver.findElements(By.className("input__control")).get(1).sendKeys("+79273569501");
+//        driver.findElement(By.className("checkbox__box")).click();
+//        driver.findElement(By.className("button")).click();
+//        String actualText = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
+//    }
 
     @Test
     public void shouldSuccessTestV2() {
